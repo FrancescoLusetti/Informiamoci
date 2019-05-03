54b328226fcd97880bc05a55b09b43516fc17c0c
Francesco Lusetti <0002luso.ciccio@gmail.com>
2019-05-03 15:02:49 +0200

    JsON

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
 let data;
 let index = 0;
-$(document).ready(function(){
 
+$(document).ready(function(){
     loadJSON();
+    //writeQuestion();
 });
 
 function loadJSON() {
-    $.getJSON("question.json",(json)=>{
+    $.getJSON("question.json",function(json){
         data = json;
     });
 }
 
-function newQuestion() {
+function writeQuestion() {
+    //$("#title").html(data[index]);
+    //$("#question").html(data[index]);
     console.log(data[index]);
 }
 
 function goBack(){
     if(index<=0) index = data.length-1;
     else index--;
-    newQuestion();
+    writeQuestion();
 }
 
 function goForward() {
     if(index>=data.length-1) index = 0;
     else index++;
-    newQuestion();
+    writeQuestion();
 }
